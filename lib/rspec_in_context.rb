7ed31d3d460d7ccc935e50af6eca130486ada594
Denis Zaratan Pasin <zaratan@hey.com>
2026-01-26 17:13:53 +0100

    Update prettier

--- lib/rspec_in_context.rb
+++ lib/rspec_in_context.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'active_support/all'
-require 'rspec_in_context/version'
-require 'rspec_in_context/in_context'
-require 'rspec_in_context/context_management'
+require "active_support/all"
+require "rspec_in_context/version"
+require "rspec_in_context/in_context"
+require "rspec_in_context/context_management"
 
 # Main wrapping module
 module RspecInContext
@@ -24,7 +24,14 @@ module RSpec
   # @param silent [Boolean] Does the in_context should wrap itself into a context block with its name
   # @param print_context [Boolean] Reverse alias of silent
   # @param block [Proc] code that will be injected later
-  def self.define_context(name, namespace: nil, ns: nil, silent: true, print_context: nil, &)
+  def self.define_context(
+    name,
+    namespace: nil,
+    ns: nil,
+    silent: true,
+    print_context: nil,
+    &
+  )
     namespace ||= ns
     silent = !print_context unless print_context.nil?
     RspecInContext::InContext.outside_define_context(name, namespace, silent, &)
